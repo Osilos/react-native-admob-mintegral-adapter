9883337b4532eb495441510b38245f66783b97b1
Osilos <f.marianacci@gmail.com>
2025-03-03 15:49:47 +0100

    chore: fix android kt script

--- android/src/main/java/com/admobmintegraladapter/AdmobMintegralAdapterModule.kt
+++ android/src/main/java/com/admobmintegraladapter/AdmobMintegralAdapterModule.kt
@@ -3,17 +3,18 @@ package com.admobmintegraladapter
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 import com.mbridge.msdk.out.MBridgeSDKFactory
+import com.mbridge.msdk.MBridgeConstans
 
 @ReactModule(name = AdmobMintegralAdapterModule.NAME)
-class AdmobMintegralAdapterModule(reactContext: ReactApplicationContext) :
+class AdmobMintegralAdapterModule(private val reactContext: ReactApplicationContext) :
   NativeAdmobMintegralAdapterSpec(reactContext) {
 
   override fun getName(): String {
     return NAME
   }
   override fun setConsent(consent: Boolean) {
-    MBridgeSDK sdk = MBridgeSDKFactory.getMBridgeSDK();
-    sdk.setConsentStatus(context, MBridgeConstans.IS_SWITCH_ON);
+    var sdk = MBridgeSDKFactory.getMBridgeSDK()
+    sdk.setConsentStatus(reactContext, MBridgeConstans.IS_SWITCH_ON)
   }
 
   companion object {
